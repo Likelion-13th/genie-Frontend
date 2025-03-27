88b2aaa41278c90e12dacf34b68d92bfbe846e91
genie525 <nlcc3333@naver.com>
2025-03-27 20:31:55 +0900

    디퓨저 경로 수정정

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from './pages/Home/Home'
 import Mypage from "./pages/Mypage/Mypage";
-import Diffuser from "./pages/ProductPage/Diffuser";
+import Diffuser from './pages/ProductPage/diffuser';
 import Perfume from "./pages/ProductPage/Perfume";
 import New from "./Pages/ProductPage/New";
 
--- src/pages/ProductPage/Diffuser.js
+++ src/pages/ProductPage/Diffuser.js
@@ -8,5 +8,4 @@ const Diffuser = () => {
     )
 }
 
-
 export default Diffuser;
