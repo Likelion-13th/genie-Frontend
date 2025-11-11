b9fe1227ae0057b7ecc7c0f72b7fdfc9b259f59e
genie525 <nlcc3333@naver.com>
2025-11-11 23:30:43 +0900

    cc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import React from "react"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from './pages/Home/Home';
@@ -13,25 +13,29 @@ import Sunglasses from './pages/ProductPage/Sunglasses';
 import Glasses from './pages/ProductPage/Sunglasses';
 import LIKELION from "./pages/Home/Home";
 import ToolBar from './components/ToolBar';
+import { CookiesProvider } from 'react-cookie';
 
 function App() {
+  const [isLogin, setIsLogin] =useState(false);
   return (
-    <Router>
-      <Header />
-      <ToolBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/mypage" element={<Mypage />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/diffuser" element={<Diffuser />} />
-        <Route path="/perfume" element={<Perfume />} />
-        <Route path="/brands" element={<Brands />} /> 
-        <Route path="/sunglasses" element={<Sunglasses />} />
-        <Route path="/glasses" element={<Glasses />} />
-        <Route path="/home" element={<LIKELION />} />      
-      </Routes>
-      <Footer />
-    </Router>
+    <CookiesProvider>
+      <Router>
+        <Header />
+        <ToolBar isLogin={isLogin} onLoginChange={setIsLogin}/>
+        <Routes>
+          <Route path="/" element={<Home onLoginChange={setIsLogin}/>} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/diffuser" element={<Diffuser />} />
+          <Route path="/perfume" element={<Perfume />} />
+          <Route path="/brands" element={<Brands />} /> 
+          <Route path="/sunglasses" element={<Sunglasses />} />
+          <Route path="/glasses" element={<Glasses />} />
+          <Route path="/home" element={<LIKELION />} />      
+        </Routes>
+        <Footer />
+      </Router>
+    </CookiesProvider>
    
   );
 }
--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,18 +1,40 @@
 import React from "react";
 import "../styles/ToolBar.css";
+import { useCookies } from "react-cookie";
+import axios from "axios";
 
-const ToolBar = () => {
+const ToolBar = ({ isLogin, onLoginChange }) => {
+  const [cookies, removeCookie] = useCookies(["accessToken"]);
+
+  const handleLogout = () => {
+    axios
+    .delete("/logout", {
+      headers: {
+        accept: "*/*",
+        Authorization: `Bearer ${cookies.accessToken}`,
+      },
+    })
+    .then(() => {})
+    .catch((err) => {
+      console.log("LOGOUT APT 요청 실패", err);
+    });
+  };
   return (
     <div className="toolbar-container">
       <img
-        src={`${process.env.PUBLIC_URL}/icon/icon_login.svg`}
+        src={isLogin 
+          ? `${process.env.PUBLIC_URL}/icon/icon_login.svg`
+          : `${process.env.PUBLIC_URL}/icon/icon_logout.svg`
+        }
         alt="login"
         className="toolbar-icon"
+        onClick={handleLoginRedirect}
       ></img>
       <img
         src={`${process.env.PUBLIC_URL}/icon/icon_recent.svg`}
-        alt="recent"
+        alt="recent" 
         className="toolbar-icon"
+        onClick={isLogin ? handleLogout : handleLoginRedirect}
       ></img>
       <img
         src={`${process.env.PUBLIC_URL}/icon/icon_up.svg`}
@@ -37,6 +59,19 @@ const MoveToTop = () => {
 const MoveToBottom = () => {
     window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
   };
+
+const handleLoginRedirect = () => {
+  const redirectUrl =
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://genie-likelion.netlify.app/";
+
+  const oauthUrl = 
+    "http://Sajang-dev-env.eba-pfhm69kf.ap-northeast-2.elasticbeanstalk.com/oauth2/start/kakao" +
+    `?redirect_uri=${encodeURIComponent(redirectUrl)}`;
+
+  window.location.href = oauthUrl;
+}
 
 export default ToolBar;
 
